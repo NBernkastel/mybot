5dc0ff9a5a27c5c011e601029b3f9ab0ec889b65
Bern <103102485+NBernkastel@users.noreply.github.com>
2022-08-20 15:05:32 +0600

    Update main.py

--- main.py
+++ main.py
@@ -4,27 +4,29 @@ from discord import utils
 from discord.utils import get
 from discord.ext.commands import Bot
 from youtube_dl import YoutubeDL
-intents = discord.Intents.all()
-client = Bot(command_prefix=';;', intents=intents)
+client = Bot(command_prefix=';;') 
 YDL_OPTIONS = {'format': 'worstaudio/best',
                'noplaylist': 'True', 'simulate': 'True', 'preferredquality': '192', 'preferredcodec': 'mp3', 'key': 'FFmpegExtractAudio'}
-@client.event
-async def on_ready():
-    await client.change_presence(status=discord.Status.online, activity=discord.Game("jojo"))
 
 @client.command(pass_context=True)
 async def play(ctx):
     print("work")
     global voice
     channel = ctx.message.author.voice.channel
+    message = ctx.message
+    cont = message.content.split(" ")
+    print(cont)
     voice = get(client.voice_clients, guild=ctx.guild)
     if voice and voice.is_connected():
         await voice.move_to(channel)
     else:
         voice = await channel.connect()
     with YoutubeDL(YDL_OPTIONS) as ydl:
-        info = ydl.extract_info(download=False,url = 'https://www.youtube.com/watch?v=A-qRH1yuqXc&list=RDGMEMJQXQAmqrnmK1SEjY_rKBGAVMA-qRH1yuqXc&start_radio=1')
+        info = ydl.extract_info(download=False,url = cont[1])
         URL = info['formats'][0]['url']
-        voice.play(discord.FFmpegPCMAudio(URL))
-
+#        voice.play(discord.FFmpegPCMAudio(URL))
+@client.event
+async def on_ready():
+    await client.change_presence(status=discord.Status.online, activity=discord.Game("jojo"))
+    print("redy")
 client.run('OTk5NjQ0NzAxMzU3NTEwNzI2.GASCob.rCYo2YngMBB6E0SsGy6hmaGmy-Mma4O1_iOyPI')
