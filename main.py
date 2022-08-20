8ec540cefee78fbcdc5ffffab0a0573fc84362df
Bern <103102485+NBernkastel@users.noreply.github.com>
2022-08-20 14:01:22 +0600

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from discord import utils
 from discord.utils import get
 from discord.ext.commands import Bot
 from youtube_dl import YoutubeDL
-intents = discord.Intents.default()
+intents = nextcord.Intents.all()
 client = Bot(command_prefix=';;', intents=intents)
 YDL_OPTIONS = {'format': 'worstaudio/best',
                'noplaylist': 'True', 'simulate': 'True', 'preferredquality': '192', 'preferredcodec': 'mp3', 'key': 'FFmpegExtractAudio'}
